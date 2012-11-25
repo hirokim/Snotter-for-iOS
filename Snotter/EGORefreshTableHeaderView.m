7509fc93af48d987e0edd74f532c9b231cc9ab30
Hiroki Matsuse <nagareha.no1@gmail.com>
2012-11-25 18:26:26 +0900

    更新日付フォーマット修正

--- Snotter/EGORefreshTableHeaderView.m
+++ Snotter/EGORefreshTableHeaderView.m
@@ -112,7 +112,7 @@
 		NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
 		[formatter setAMSymbol:@"AM"];
 		[formatter setPMSymbol:@"PM"];
-		[formatter setDateFormat:@"MM/dd/yyyy hh:mm:a"];
+		[formatter setDateFormat:@"yyyy/MM/dd hh:mm:a"];
 		_lastUpdatedLabel.text = [NSString stringWithFormat:@"Last Updated: %@", [formatter stringFromDate:date]];
 		[[NSUserDefaults standardUserDefaults] setObject:_lastUpdatedLabel.text forKey:@"EGORefreshTableView_LastRefresh"];
 		[[NSUserDefaults standardUserDefaults] synchronize];
